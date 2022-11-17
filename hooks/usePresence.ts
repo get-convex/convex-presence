222e8b40383c2d3c0b109fb80a2e81c4baa5af49
Ian Macartney <ian@convex.dev>
2022-11-16 16:42:47 -0800

    use single flight

--- hooks/usePresence.ts
+++ hooks/usePresence.ts
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { Id } from '../convex/_generated/dataModel';
 import { useQuery, useMutation } from '../convex/_generated/react';
-import useLatestValue from './useLatestValue';
+import useSingleFlight from './useSingleFlight';
 
 export default (location: string, recencyMs: number = 10000) => {
-  const [presenceId, setPresenceId] = useState<Id<'presence'>>();
+  let [presenceId, setPresenceId] = useState<Id<'presence'>>();
   let presence = useQuery('getPresence', location, recencyMs);
   presence = presence?.filter((p) => p.updated > Date.now() - recencyMs);
   if (presence && presenceId) {
@@ -14,25 +14,19 @@ export default (location: string, recencyMs: number = 10000) => {
   const updatePresence = useMutation('updatePresence');
   const createPresence = useMutation('createPresence');
 
-  const [getValue, update] = useLatestValue<{}>();
+  const updateSF = useSingleFlight(
+    useCallback(
+      async (data: {}) => {
+        if (!presenceId) {
+          presenceId = await createPresence(location, data);
+          setPresenceId(presenceId);
+        } else {
+          await updatePresence(presenceId, data);
+        }
+      },
+      [presenceId, setPresenceId, updatePresence]
+    )
+  );
 
-  useEffect(() => {
-    let stop = false;
-    const init = async () => {
-      const initialData = await getValue();
-      const presenceId = await createPresence(location, initialData);
-      setPresenceId(presenceId);
-      while (!stop) {
-        const data = await getValue();
-        if (stop) break;
-        await updatePresence(presenceId, data);
-      }
-    };
-    init();
-    return () => {
-      stop = true;
-    };
-  }, []);
-
-  return [presence, update] as const;
+  return [presence, updateSF] as const;
 };
---
+++ hooks/useSingleFlight.ts
@@ -0,0 +1,35 @@
+import { useCallback, useRef } from 'react';
+
+export default function useSingleFlight<
+  F extends (...args: any[]) => Promise<any>
+>(fn: F) {
+  const flightStatus = useRef({
+    inFlight: false,
+    upNext: null as null | { resolve: any; reject: any; args: Parameters<F> },
+  });
+
+  return useCallback(
+    (...args: Parameters<F>) => {
+      if (!flightStatus.current.inFlight) {
+        flightStatus.current.inFlight = true;
+        const firstReq = fn(...args);
+        (async (_) => {
+          await firstReq;
+          while (flightStatus.current.upNext) {
+            let cur = flightStatus.current.upNext;
+            flightStatus.current.upNext = null;
+            await fn(...cur.args)
+              .then(cur.resolve)
+              .catch(cur.reject);
+          }
+          flightStatus.current.inFlight = false;
+        })();
+        return firstReq;
+      }
+      return new Promise((resolve, reject) => {
+        flightStatus.current.upNext = { resolve, reject, args };
+      });
+    },
+    [fn]
+  );
+}
